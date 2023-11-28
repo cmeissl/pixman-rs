a14c39452047b9b45dd7cac601dc815843885f11
Christian Meissl <cmeissl@novomatic.com>
2023-11-28 14:13:34 +0100

    de-duplicate region and transform

--- pixman/src/vector.rs
+++ pixman/src/vector.rs
@@ -1,10 +1,12 @@
 use crate::{ffi, Fixed};
 
+/// A single vector
 #[derive(Debug, Clone, Copy)]
 #[repr(transparent)]
 pub struct Vector(ffi::pixman_vector_t);
 
 impl Vector {
+    /// Initialize a vector from the provided values
     #[inline]
     pub fn new<T: Into<Fixed> + Copy>(vector: [T; 3]) -> Self {
         Self(ffi::pixman_vector {
@@ -16,14 +18,17 @@ impl Vector {
         })
     }
 
+    /// Access the x component of this vector
     pub fn x(&self) -> Fixed {
         Fixed::from_raw(self.0.vector[0])
     }
 
+    /// Access the y component of this vector
     pub fn y(&self) -> Fixed {
         Fixed::from_raw(self.0.vector[1])
     }
 
+    /// Access the z component of this vector
     pub fn z(&self) -> Fixed {
         Fixed::from_raw(self.0.vector[2])
     }
@@ -51,6 +56,62 @@ impl From<ffi::pixman_vector_t> for Vector {
 impl From<Vector> for ffi::pixman_vector_t {
     #[inline]
     fn from(value: Vector) -> Self {
+        value.0
+    }
+}
+
+/// Floating-point vector
+#[derive(Debug, Clone, Copy)]
+#[repr(transparent)]
+pub struct FVector(ffi::pixman_f_vector_t);
+
+impl FVector {
+    /// Initialize the vector from the specified values
+    #[inline]
+    pub fn new(v: [f64; 3]) -> Self {
+        Self(ffi::pixman_f_vector_t {
+            v: [v[0], v[1], v[2]],
+        })
+    }
+
+    /// Access the x component of this vector
+    pub fn x(&self) -> f64 {
+        self.0.v[0]
+    }
+
+    /// Access the y component of this vector
+    pub fn y(&self) -> f64 {
+        self.0.v[1]
+    }
+
+    /// Access the z component of this vector
+    pub fn z(&self) -> f64 {
+        self.0.v[2]
+    }
+
+    #[inline]
+    pub(crate) fn as_mut_ptr(&mut self) -> *mut ffi::pixman_f_vector_t {
+        &mut self.0 as *mut ffi::pixman_f_vector_t
+    }
+}
+
+impl From<[f64; 3]> for FVector {
+    #[inline]
+    fn from(value: [f64; 3]) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<ffi::pixman_f_vector_t> for FVector {
+    #[inline]
+    fn from(value: ffi::pixman_f_vector_t) -> Self {
+        Self(value)
+    }
+}
+
+impl From<FVector> for ffi::pixman_f_vector_t {
+    #[inline]
+    fn from(value: FVector) -> Self {
         value.0
     }
 }
